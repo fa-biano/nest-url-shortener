5b0de9018e1033817e23bdbb05e275c43f0607c9
Fa-biano <fbiano.souza@gmail.com>
2025-08-07 00:51:40 -0300

    feat: added UrlModule

---
+++ src/url/url.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common'
+import { UrlController } from './url.controller'
+import { UrlService } from './url.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { UrlEntity } from './url.entity'
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UrlEntity])],
+  controllers: [UrlController],
+  providers: [UrlService],
+})
+export class UrlModule {}
