b521f403f6edad018f6105747ffd6e5e50fef820
Fa-biano <fbiano.souza@gmail.com>
2025-08-09 01:53:13 -0300

    perf: refact createShortUrl to avoid infinite loop

--- src/url/url.service.ts
+++ src/url/url.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common'
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
@@ -21,22 +26,39 @@ export class UrlService {
     return code
   }
 
+  private isDatabaseError(error: any): error is { code: string } {
+    return typeof error === 'object' && error !== null && 'code' in error
+  }
+
   async createShortUrl(originalUrl: URL): Promise<URL> {
     let urlShortCode: string = ''
-    let exists = true
+    let attempts = 0
+    const maxAttempts = 5
+    const stringUrl = originalUrl.toString()
+    const PG_UNIQUE_CONSTRAINT_VIOLATION = '23505'
 
-    while (exists) {
-      urlShortCode = this.generateShortCode()
-      const saveShortCode = await this.urlRepository.findOne({ where: { urlShortCode } })
-      exists = saveShortCode ? true : false
+    while (attempts < maxAttempts) {
+      try {
+        urlShortCode = this.generateShortCode()
+        await this.urlRepository.save({ originalUrl: stringUrl, urlShortCode })
+
+        const apiHost = this.configService.get<string>('API_HOST', 'http://localhost:3001')
+        const shortenerUrl: URL = new URL(`${apiHost}/${urlShortCode}`)
+        return shortenerUrl
+      } catch (error) {
+        if (this.isDatabaseError(error) && error.code === PG_UNIQUE_CONSTRAINT_VIOLATION) {
+          attempts += 1
+          continue
+        }
+
+        console.error('[UrlService] Error on createShortUrl:', JSON.stringify(error))
+        throw new InternalServerErrorException('An unexpected error occurred.')
+      }
     }
 
-    const stringUrl = originalUrl.toString()
-    await this.urlRepository.save({ originalUrl: stringUrl, urlShortCode })
-
-    const apiHost = this.configService.get<string>('API_HOST', 'http://localhost:3001')
-    const shortenerUrl: URL = new URL(`${apiHost}/${urlShortCode}`)
-    return shortenerUrl
+    throw new ConflictException(
+      `Failed to create ShortUrl after ${maxAttempts} attempts. Try again later.`,
+    )
   }
 
   async findUrlByShortCode(urlShortCode: string): Promise<UrlEntity> {
