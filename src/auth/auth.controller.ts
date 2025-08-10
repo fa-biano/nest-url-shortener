e47cf26674e0d284df40c23679eeefd57957b358
Fa-biano <fbiano.souza@gmail.com>
2025-08-10 02:51:41 -0300

    feat: created AuthController

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,16 @@
+// src/auth/auth.controller.ts
+import { Controller, Post, Body, HttpCode } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { LoginDto } from './dtos/login.dto'
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  @HttpCode(200)
+  async login(@Body() loginDto: LoginDto) {
+    const token = await this.authService.login(loginDto)
+    return { token }
+  }
+}
