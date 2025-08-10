f6fcba7b303eda0a4f2b16ed47556e73025c8b18
Fa-biano <fbiano.souza@gmail.com>
2025-08-10 17:53:17 -0300

    feat: created JwtStrategy class

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -4,6 +4,7 @@ import { Strategy, ExtractJwt } from 'passport-jwt'
 import { AuthService } from './auth.service'
 import { ConfigService } from '@nestjs/config'
 import { JwtPayload } from './interfaces/jwt-payload.interface'
+import { UserResponseDto } from '../user/dtos/user-response.dto'
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -12,13 +13,13 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     private readonly configService: ConfigService,
   ) {
     super({
-      jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+      jwtFromRequest: ExtractJwt.fromHeader('token'),
       ignoreExpiration: false,
       secretOrKey: configService.get<string>('JWT_SECRET') || '',
     })
   }
 
-  async validate(payload: JwtPayload) {
+  async validate(payload: JwtPayload): Promise<UserResponseDto> {
     const user = await this.authService.validateUserByPayload(payload)
     if (!user) throw new UnauthorizedException()
     return user
