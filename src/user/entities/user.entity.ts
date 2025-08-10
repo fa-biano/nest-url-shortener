48048ef7a1ca9807dd62d033b5878035d6847c03
Fa-biano <fbiano.souza@gmail.com>
2025-08-09 22:35:06 -0300

    feat: created UserEntity

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,34 @@
+import { UrlEntity } from 'src/url/entities/url.entity'
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  OneToMany,
+} from 'typeorm'
+
+@Entity('users')
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column({ unique: true, length: 255 })
+  email: string
+
+  @Column({ select: false, length: 255 })
+  passwordHash: string
+
+  @CreateDateColumn()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  updatedAt: Date
+
+  @DeleteDateColumn({ type: 'timestamp with time zone', nullable: true })
+  deletedAt: Date | null
+
+  @OneToMany(() => UrlEntity, (url) => url.user)
+  urls: UrlEntity[]
+}
