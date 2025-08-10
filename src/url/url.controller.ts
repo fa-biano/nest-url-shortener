d229e1b377b6f88405eee855af13f43d67bf1daf
Fa-biano <fbiano.souza@gmail.com>
2025-08-10 17:58:52 -0300

    feat: added UseGuards to createShortUrl method

--- src/url/url.controller.ts
+++ src/url/url.controller.ts
@@ -1,8 +1,21 @@
-import { Body, Controller, Get, HttpCode, Param, Post, Res, ValidationPipe } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common'
 import express from 'express'
 import { UrlService } from './url.service'
 import { CreateUrlDto } from './dtos/create-url.dto'
 import { UrlShortCodeDto } from './dtos/url-short-code.dto'
+import { UserResponseDto } from 'src/user/dtos/user-response.dto'
+import { OptionalJwtAuthGuard } from 'src/auth/guards/optional-jwt-auth.guard'
 
 @Controller('')
 export class UrlController {
@@ -10,8 +23,13 @@ export class UrlController {
 
   @Post()
   @HttpCode(201)
-  async createShortUrl(@Body() { originalUrl }: CreateUrlDto): Promise<{ shortUrl: URL }> {
-    const shortUrl = await this.urlService.createShortUrl(originalUrl)
+  @UseGuards(OptionalJwtAuthGuard)
+  async createShortUrl(
+    @Req() req: express.Request,
+    @Body() { originalUrl }: CreateUrlDto,
+  ): Promise<{ shortUrl: URL }> {
+    const authUser = req.user as UserResponseDto
+    const shortUrl = await this.urlService.createShortUrl(originalUrl, authUser)
     return { shortUrl }
   }
 
