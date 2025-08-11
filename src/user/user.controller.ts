b4d4731b1974c80903854076d2f7080b2089684c
Fa-biano <fbiano.souza@gmail.com>
2025-08-10 22:46:31 -0300

    feat: added findUrlsByUserId method to UserController

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common'
+import { Body, Controller, Get, HttpCode, Param, Post, UseGuards } from '@nestjs/common'
 import { UserService } from './user.service'
 import { CreateUserDto } from './dtos/create-user.dto'
 import { UserResponseDto } from './dtos/user-response.dto'
 import { UserIdDto } from './dtos/user-id.dto'
+import { UserEntity } from './entities/user.entity'
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard'
 
 @Controller('user')
 export class UserController {
@@ -18,6 +20,13 @@ export class UserController {
   @Get(':id')
   async findUserById(@Param() { id }: UserIdDto): Promise<UserResponseDto> {
     const user = await this.userService.findUserById(id)
+    return user
+  }
+
+  @Get(':id/urls')
+  @UseGuards(JwtAuthGuard)
+  async findUrlsByUserId(@Param() { id }: UserIdDto): Promise<UserEntity> {
+    const user = await this.userService.findUrlsByUserId(id)
     return user
   }
 }
